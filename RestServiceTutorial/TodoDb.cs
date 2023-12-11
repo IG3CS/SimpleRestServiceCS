6431d786aae917990026b310974856e36c38cacb
Ian Gigante <ian.gigante@3cloudsolutions.com>
2023-12-11 21:25:07 +0800

    Initial Commit

---
+++ RestServiceTutorial/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿namespace RestServiceTutorial
+{
+    using System.Collections.Generic;
+    using Microsoft.EntityFrameworkCore;
+
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
